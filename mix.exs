21732c94f00c80c969d6b3113dfddb585e2c4edf
mehdivk <valikhani@sirangweb.com>
2017-06-18 19:54:55 +1000

    fixed a bug!

--- mix.exs
+++ mix.exs
@@ -3,10 +3,12 @@ defmodule Issues.Mixfile do
 
   def project do
     [app: :issues,
+     escript: escript_config,
      version: "0.1.0",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
+     source_url: "https://github.com/mehdivk/issues",
      deps: deps()]
   end
 
@@ -15,7 +17,7 @@ defmodule Issues.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     # Specify extra applications you'll use from Erlang/Elixir
-    [extra_applications: [:logger]]
+    [extra_applications: [:logger, :httpoison]]
   end
 
   # Dependencies can be Hex packages:
@@ -28,6 +30,15 @@ defmodule Issues.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [
+      {:httpoison, "~> 0.9"},
+      {:poison, "~> 2.2"},
+      {:ex_doc, "~> 0.12"},
+      {:earmark, "~> 1.0", override: true}
+    ]
+  end
+
+  defp escript_config do
+    [ main_module: Issues.CLI ]
   end
 end
--- test/cli_test.exs
+++ test/cli_test.exs
@@ -2,7 +2,10 @@ defmodule CliTest do
   use ExUnit.Case
   doctest Issues
 
-  import Issues.CLI, only: [ parse_args: 1 ]
+  import Issues.CLI, only: [
+    parse_args: 1,
+    sort_into_ascending_order: 1
+  ]
 
   test "when provided with -h or --help, returns :help atom" do
     assert parse_args(["-h", "anything", "else"]) == :help
@@ -24,5 +27,18 @@ defmodule CliTest do
     { _, _, count } = parse_args(argv)
 
     assert count == 4
+  end
+
+  test "sort ascending orders the correct way" do
+    result = sort_into_ascending_order(fake_created_at_list(["c", "a", "b"]))
+
+    issues = for issue <- result, do: Map.get(issue, "created_at")
+
+    assert issues == ~w{a b c}
+  end
+
+  defp fake_created_at_list(values) do
+    for value <- values,
+    do: %{"created_at" => value, "other_data" => "XXX"}
   end
 end
